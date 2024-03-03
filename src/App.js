c53b6a4adb8af505a0a65b2e1080047e1a93c95a
SuperSlowStarter <cleangabby@gmail.com>
2024-03-03 23:21:48 +0900

    Update toDo list [0303]

--- src/App.js
+++ src/App.js
@@ -2,17 +2,37 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event) => setToDo(event.target.value);
-  console.log(toDo);
+  const [toDo, setToDo] = useState(""); //변수 toDo와 modifier 인 setToDo
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value); //input에 값이 변했을 때 그 값을 받아서 toDo에 넣어줌
+  const onSubmit = (event) => {
+    event.preventDefault(); //이것이 없다면 button의 submit에 의 해 페이지가 새로고침 되어버린다.
+    if (toDo === "") {
+      //input에 아무것도 없다면 동작하지 않을 것
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
   return (
     <div>
-      <input
-        onChange={onChange}
-        value={toDo}
-        type="text"
-        placeholder="Write your to do..."
-      ></input>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        ></input>
+        <button>Add To Do</button> {/*버튼이 form을 submit 함*/}
+        <hr />
+        <ul>
+          {toDos.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
